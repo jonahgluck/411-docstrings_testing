4fde763651c760d411ca918bca552cfb05250d84
katyabalin <katya.balin@gmail.com>
2024-11-08 09:54:36 -0500

    Added docstrings to random_utils

--- meal_max/meal_max/utils/random_utils.py
+++ meal_max/meal_max/utils/random_utils.py
@@ -10,6 +10,21 @@ configure_logger(logger)
 def get_random() -> float:
     url = "https://www.random.org/decimal-fractions/?num=1&dec=2&col=1&format=plain&rnd=new"
 
+""" Retrieves a random decimal number from random.org
+    Sends a GET request to the random.org API to obtain a random decimal fraction. 
+    Logs the process and handles any errors that may occur, raising an exception if the 
+    request fails or if the response cannot be parsed to a float.
+
+    Returns: 
+        float: a random decimal number provided by random.org
+    Raises:
+        ValueError: Raised if the response from random.org is not a valid float.
+        RuntimeError: Raised if the request times out or fails due to a connection issue.
+    Ex:
+        >>> random_value = get_random()
+        >>> print(f"Random value: {random_value}"
+"""
+    
     try:
         # Log the request to random.org
         logger.info("Fetching random number from %s", url)
